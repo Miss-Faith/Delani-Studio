989b208b2aa1ba308e5049cf6d35fb35fe1806c9
Faith Mwangi <faith.mwangi@student.moringaschool.com>
2022-03-13 23:37:50 +0300

    Include jquerry toggle

--- js/front_end_main.js
+++ js/front_end_main.js
@@ -1,1 +1,17 @@
-.
+$(document).ready(function() {
+
+  $("#design").click(function() {
+    $("#designimage").toggle();
+    $("#designtext").toggle();
+  });
+  $("#development").click(function() {
+    $("#developmentimage").toggle();
+    $("#developmenttext").toggle();
+  });
+  $("#product").click(function() {
+    $("#productimage").toggle();
+    $("#producttext").toggle();
+  });
+
+
+});
