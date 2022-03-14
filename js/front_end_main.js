12846b386334cc0e29aa539af6d68961aabc86aa
Faith Mwangi <faith.mwangi@student.moringaschool.com>
2022-03-14 08:19:41 +0300

    Add Mailchimp

--- js/front_end_main.js
+++ js/front_end_main.js
@@ -21,8 +21,20 @@ $(document).ready(function() {
   });
 
   $('#submit').click(function() {
-    var name = $("#name").val();
+    var name = $("#mce-FNAME").val();
     alert("Hi "+ name + " we have received your message. Thank you for reaching out to us. We'll get back to you soonest.")
+
+  /*  (function($) {
+      window.fnames = new Array();
+      window.ftypes = new Array();
+      fnames[0]='EMAIL';
+      ftypes[0]='email';
+      fnames[1]='FNAME';
+      ftypes[1]='text';
+      fnames[3]='MMERGE3';
+      ftypes[3]='text';
+    }
+    (jQuery));var $mcj = jQuery.noConflict(true);</script>*/
   });
 
 });
