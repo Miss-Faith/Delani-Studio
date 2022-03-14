afe2b37eeb7afb94924636f9bc33b984506182bc
Faith Mwangi <faith.mwangi@student.moringaschool.com>
2022-03-14 11:52:02 +0300

    Update image descriptions and pop-up notification

---
+++ js/main.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+
+  $("#design").click(function() {
+    $("#designimage").toggle();
+    $("#designtext").toggle();
+  });
+  $("#development").click(function() {
+    $("#developmentimage").toggle();
+    $("#developmenttext").toggle();
+  });
+  $("#product").click(function() {
+    $("#productimage").toggle();
+    $("#producttext").toggle();
+  });
+
+  $(".hover").hover(function() {
+    $(this).animate({opacity:'1'});
+  },
+    function(){
+      $(this).animate({opacity:'0'});
+  });
+
+});
